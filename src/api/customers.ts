7e678a0d21838bb3de87e56c06afe11baae06950
Dhanalakshmi-Chandrasekar <dhanalakshmi.chandrasekar@nimircorp.com>
2026-01-08 16:14:19 +0530

    Update API_BASE to new Azure endpoint

--- src/api/customers.ts
+++ src/api/customers.ts
@@ -1,8 +1,9 @@
 // src/api/customers.ts
-const API_BASE = "http://127.0.0.1:9002";
+const API_BASE = "cravecallcateringbk-hrgjcyd3aeaxc3dz.canadacentral-01.azurewebsites.net";
 
 export async function fetchCustomersSummary() {
   const res = await fetch(`${API_BASE}/customers/summary`);
   if (!res.ok) throw new Error("Failed to fetch customers summary");
   return await res.json();
 }
+
